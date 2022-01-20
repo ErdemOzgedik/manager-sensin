63b43e5f3d9b0271f5eef10c2bd5bd1d99654800
erdem <erdem@erdem-MacBook-Pro.local>
2022-01-20 13:52:17 +0300

    manager create and transfer process completed

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -23,7 +24,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 		filter := helper.AddFilterViaFields(&constant.Filter{
 			Overall: []int{87, 99},
 		})
-		players, err = helper.SearchByFilter(filter)
+		players, err = helper.SearchPlayerByFilter(filter)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
@@ -51,7 +52,7 @@ func searchPlayer(w http.ResponseWriter, r *http.Request) {
 	}
 	filter := helper.AddFilterViaFields(&f)
 
-	players, err := helper.SearchByFilter(filter)
+	players, err := helper.SearchPlayerByFilter(filter)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
@@ -80,7 +81,7 @@ func randomPlayer(w http.ResponseWriter, r *http.Request) {
 	err = helper.GetRedisData(rdb, key, &players)
 	if err == redis.Nil {
 		filter := helper.AddFilterViaFields(&f)
-		players, err = helper.SearchByFilter(filter)
+		players, err = helper.SearchPlayerByFilter(filter)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
@@ -108,6 +109,108 @@ func randomPlayer(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+func createManager(w http.ResponseWriter, r *http.Request) {
+	var t constant.Manager
+	err := json.NewDecoder(r.Body).Decode(&t)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	helper.CreateManager(t)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Manager created!!!"))
+}
+
+func addPlayer(w http.ResponseWriter, r *http.Request) {
+	var pt constant.PlayerTransfer
+
+	err := json.NewDecoder(r.Body).Decode(&pt)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	managerID, err := primitive.ObjectIDFromHex(pt.Manager)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	playerID, err := primitive.ObjectIDFromHex(pt.Player)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	manager, err := helper.GetManagerByID(managerID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	player, err := helper.GetPlayerByID(playerID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	manager.AddPlayer(player)
+
+	updateResult, err := helper.UpdateManager(&manager)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Println("Updated Count", updateResult.ModifiedCount)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(manager)
+}
+
+func deletePlayer(w http.ResponseWriter, r *http.Request) {
+	var pt constant.PlayerTransfer
+
+	err := json.NewDecoder(r.Body).Decode(&pt)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	managerID, err := primitive.ObjectIDFromHex(pt.Manager)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	playerID, err := primitive.ObjectIDFromHex(pt.Player)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	manager, err := helper.GetManagerByID(managerID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	manager.DeletePlayer(playerID)
+
+	updateResult, err := helper.UpdateManager(&manager)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Println("Updated Count", updateResult.ModifiedCount)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(manager)
+}
+
 func main() {
 	var port string
 	var err error
@@ -126,6 +229,11 @@ func main() {
 	router.HandleFunc("/", home)
 	router.HandleFunc("/search", searchPlayer).Methods("POST", "OPTIONS")
 	router.HandleFunc("/random", randomPlayer).Methods("POST", "OPTIONS")
+
+	//manager endpoints
+	router.HandleFunc("/createManager", createManager).Methods("POST", "OPTIONS")
+	router.HandleFunc("/addPlayer", addPlayer).Methods("POST", "OPTIONS")
+	router.HandleFunc("/deletePlayer", deletePlayer).Methods("POST", "OPTIONS")
 
 	log.Fatal(http.ListenAndServe(":"+port, handlers.CORS(header, methods, origins)(router)))
 }
